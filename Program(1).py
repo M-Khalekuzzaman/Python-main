b227140e54306d99d4f6d209cbd463281f094bdd
Kaochar <94604055+M-Khalekuzzaman@users.noreply.github.com>
2021-11-20 16:46:26 +0600

    Add files via upload

---
+++ Program(1).py
@@ -0,0 +1,65 @@
+  ##########  [Day1]  ##########
+'''
+1)Variable
+2)Data-Type
+'''
+
+#string data type
+name = "Khalekuzzaman"
+print(name)
+Import ='Kaochar'
+print(Import)
+
+#Float data type
+age = 21.5
+Data = type(age)
+print(Data)
+
+#Complex data type
+t = 5 + 4j
+print(type(t))
+
+#integer data type
+hight = 5
+print(hight)
+
+#String concatination
+firstName = 'Khalekuzzaman'
+nickName = 'Kaochar'
+print("It's me Khalekuzzaman kaochar") ##('It\'s me Khalekuzzaman kaochar)
+
+print(firstName +" "+ nickName)
+print(firstName.lower())
+
+#Some basic operation
+a= 20
+b =10
+
+sum = a + b
+sub = a - b
+div = int(a / b)
+mul = a * b
+rem = a % b
+print(div)
+print(rem)
+
+num1 = 20
+num2 = 5
+divi = num1/num2
+print(type(divi))
+
+z = 5 + 6j
+z1 = z.imag
+print(z1)
+
+_ = "Python Bootcamp"
+print(_)
+
+x = 10
+y = 12
+z = 16
+output = x + y * x + z / x
+print(int(output))
+print(bool(output))
+
+
